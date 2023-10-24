0a42d8e3eafd67dad8343b13026a671281213b8d
gusrms2142 <gusrms2142@naver.com>
2023-10-24 09:51:39 +0900

    v week09-0.2

--- week09/main.go
+++ week09/main.go
@@ -1,14 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func swap(n1 *int, n2 *int) {
+	temp := *n1
+	*n1 = *n2
+	*n2 = *&temp
+}
 
 func main() {
-	var a int = 10
-	var pa *int
-	pa = &a
-	fmt.Println(&a, a)
-	fmt.Println(pa, *pa)
-	fmt.Println(&pa)
-	fmt.Printf("%T %T\n", a, pa)
+	a := 10
+	b := 20
+	c := &a
+
+	fmt.Printf("%T\n", c)
+
+	fmt.Println(a, b)
 
 }
