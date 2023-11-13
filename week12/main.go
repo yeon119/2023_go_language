f686f06c1a218e3c863c5567c9ed2cbbc05b4b11
gusrms2142 <gusrms2142@naver.com>
2023-11-13 10:43:49 +0900

    week12.0.3

--- week12/main.go
+++ week12/main.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main() {
 
-	s := make([]int, 5)
+	s := []int{0, 0, 0, 0, 0}
 	for _, value := range s {
 		fmt.Println(value)
 	}
+
+	// s := make([]int, 5)
+	// for _, value := range s {
+	// 	fmt.Println(value)
+	// }
 
 	// var s []int
 	// s = make([]int, 5)
