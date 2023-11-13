ded254fe7e2b86e79da11a62ffd0cdeb878f4015
gusrms2142 <gusrms2142@naver.com>
2023-11-13 10:56:45 +0900

    week12.0.4

--- week12/main.go
+++ week12/main.go
@@ -1,14 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
 	s := []int{0, 0, 0, 0, 0}
+	s[4] = 99
+	s[2] = 11
 	for _, value := range s {
 		fmt.Println(value)
 	}
+	//fmt.Println("+-=")
+	copyS := s[1:4]
+	for _, value := range copyS {
+		fmt.Println(value)
+	}
 
+	for i := 0; i < len(s); i++ {
+		fmt.Println(s[i])
+	}
 	// s := make([]int, 5)
 	// for _, value := range s {
 	// 	fmt.Println(value)
