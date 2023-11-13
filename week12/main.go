31aa483fec476eff12e3dcfb1852790ad8c45df2
gusrms2142 <gusrms2142@naver.com>
2023-11-13 11:56:21 +0900

    week12.0.7

--- week12/main.go
+++ week12/main.go
@@ -5,37 +5,14 @@ import (
 )
 
 func main() {
-
-	s := []int{0, 0, 0, 0, 0}
-	s[4] = 99
-	s[2] = 11
-	for _, value := range s {
-		fmt.Println(value)
-	}
-	//fmt.Println("+-=")
-	copyS := s[1:4]
-	for _, value := range copyS {
-		fmt.Println(value)
-	}
-
-	for i := 0; i < len(s); i++ {
-		fmt.Println(s[i])
-	}
-
-	test := [3]string{"inha", "go", "student"}
-	testS := test[:2]
-	fmt.Println(len(testS))
-
-	test2S := test[1:]
-	fmt.Println(test2S[1])
-	// s := make([]int, 5)
-	// for _, value := range s {
-	// 	fmt.Println(value)
-	// }
-
-	// var s []int
-	// s = make([]int, 5)
-	// for _, value := range s {
-	// 	fmt.Println(value)
-	// }
+	a := make([]string, 4, 5)
+	a[0] = "a"
+	a[2] = "c"
+	a[3] = "d"
+	as := a[0:2]
+	c := append(a, "x", "y")
+	fmt.Println(a, len(a), cap(a))
+	fmt.Println(a, len(a), cap(a))
+	fmt.Println(c, len(c), cap(c))
+	as[1] = "z"
 }
