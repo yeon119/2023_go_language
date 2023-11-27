980645e119703ebbd0f0e26cf28c37347cdfbfa4
gusrms2142 <gusrms2142@naver.com>
2023-11-27 11:27:39 +0900

    week14.0.2

--- week14/main2.go
+++ week14/main2.go
@@ -3,14 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var games map[int]string
-	games = make(map[int]string)
-	games[456] = "성기훈"
-	games[218] = "박해수"
-	games[067] = "감새벽"
-	games[001] = "오일남"
-	games[199] = "알리"
-	games[101] = "아이오아이"
+	// var games map[int]string
+	// games = make(map[int]string)
+	games := map[int]string{456: "성기훈",
+		218: "박해수",
+		067: "감새벽",
+		001: "오일남",
+		199: "알리",
+		101: "아이오아이",
+	}
+	// games[456] = "성기훈"
+	// games[218] = "박해수"
+	// games[067] = "감새벽"
+	// games[001] = "오일남"
+	// games[199] = "알리"
+	// games[101] = "아이오아이"
 
 	for _, v := range games {
 		fmt.Println(v)
